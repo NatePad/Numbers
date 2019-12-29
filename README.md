365d35a3444517216ca16bc20b21218dbe042689
Nathan Sartain <nathanmsartain@gmail.com>
2019-12-28 20:27:35 -0500

    Added parameters for number of dice and number of sides on the dice.

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Nate's Calculator and Random Number Generator
 
 As a board gamer, I'm making this app in order to crate a random number generator, which is frequently used in various board games in the form of dice rolls and card shuffles. The live version of this app is hosted by GitHub pages at [https://natepad.github.io/Random-Number-Generator/](natepad.github.io/Random-Number-Generator).
-
-## Work in Progress
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,17 @@ $(document).ready(function() {
 $(window).on('load', function(){
   // console.log('All assets have been loaded');
 });
+
+$('#dice-roll').on('click', function(e) {
+  e.preventDefault();
+  let thisRoll = '';
+
+  for (let i = 0; i < $('#dice-count').val(); i++) {
+    thisRoll += Math.ceil(Math.random() * $('#dice-sides').val());
+    if (i+1 < $('#dice-count').val()) {
+      thisRoll += ', ';
+    }
+  }
+
+  $('#roll-result').val(thisRoll);
+});
